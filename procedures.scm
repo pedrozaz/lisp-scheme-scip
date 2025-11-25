f1e864c93b724247f40acafd92b38b2cf9f61a0d
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-25 16:54:48 -0300

    Cap 1.1.5

--- procedures.scm
+++ procedures.scm
@@ -10,6 +10,7 @@
 area
 circumference
 
+
 (square 21)
 (square (+ 2 5))
 (square (square 3))
@@ -18,6 +19,35 @@ circumference
   (+ (square x) (square y)))
 (sum-of-squares 3 4)
 
+
 (define (f a)
   (sum-of-squares (+ a 1) (* a 2)))
+
 (f 5)
+(sum-of-squares (+ 5 1) (* 5 2))
+(+ (square (+ 5 1)) (square (* 5 2)))
+(+ (* (+ 5 1) (+ 5 1)) (* (* 5 2) (* 5 2)))
+(+ (* 6 6) (* 10 10))
+(+ 36 100)
+136
+
+
+(define (myabs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(define (myabs2 x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(define (myabs3 x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(define x 2)
+(and (> x 5) (< x 10))
+
+
+
